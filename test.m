a059fa555df3556c625536ee01f854a284247053
Long <93868494+iamtrusters@users.noreply.github.com>
2022-05-29 14:51:39 +0700

    Add files via upload

---
+++ test.m
@@ -0,0 +1,38 @@
+clear all
+
+n = 50; % 
+
+% Setting initial times
+clipLength = zeros(1,n);
+initialTime = zeros(1,n);
+for i = 1:n
+    toRead = strcat('songDatabase/', num2str(i),'.mat');
+    load(toRead, '-mat');   
+    clipLength(i) = length(y)/Fs;
+    initialTime(i) = randi(round(clipLength(i)-10));
+end
+
+% Testing algorithms with varying times
+songID = zeros(1,n);
+for i = 1:n
+    id = num2str(i);
+    timeTaken(i) = 0;
+    
+    while songID(i) == 0
+        timeTaken(i) = timeTaken(i) + 1;
+        toRead = strcat('songDatabase/', num2str(i),'.mat');
+        load(toRead, '-mat');  
+        if timeTaken(i) >  10
+            timeTaken(i) = timeTaken(i) - 1
+            break
+        end
+        yInput = y((initialTime(i)*Fs:initialTime(i)*Fs +  timeTaken(i)*Fs), :);
+        songID(i) = mainabc123(yInput, Fs); % Your code will be used here instead of abc123
+    end
+end
+
+% Results
+accuracy = sum(songID == [1:n])/n
+averageTimeTaken = sum(timeTaken)/length(songID)
+points = 3*sum(songID == [1:n]) - nnz(songID);
+score = points/averageTimeTaken
